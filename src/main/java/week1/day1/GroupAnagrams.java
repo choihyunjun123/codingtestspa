3645e05400028ee331c66ed541ac5ab859b5b8be
HYUNJUN\sajun <sajun28@naver.com>
2024-01-07 13:34:44 +0900

    Feat: "groupAnagrams"

--- src/main/java/week1/day1/GroupAnagrams.java
+++ src/main/java/week1/day1/GroupAnagrams.java
@@ -1,7 +1,25 @@
 package week1.day1;
 
+import java.util.*;
+
 public class GroupAnagrams {
     public static void main(String[] args) {
+        String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        HashMap<String, List<String>> map = new HashMap<>();
+        List<List<String>> answer = new ArrayList<>();
+        for (String a : strs) {
+            char[] chars = a.toCharArray();
+            Arrays.sort(chars);
+            String sorted = new String(chars);
 
+            if (map.containsKey(sorted)) {
+                map.get(sorted).add(a);
+            } else {
+                List<String> newList = new ArrayList<>();
+                newList.add(a);
+                map.put(sorted, newList);
+            }
+        }
+        System.out.println(new ArrayList<>(map.values()));
     }
 }
