299b3413855c0eddabf7e58df21161bde6845f6a
HYUNJUN\sajun <sajun28@naver.com>
2024-01-07 14:13:22 +0900

    Feat: "baek_10809"

---
+++ src/main/java/week1/day1/Baek_10809.java
@@ -0,0 +1,22 @@
+package week1.day1;
+
+import java.util.*;
+
+public class Baek_10809 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        List<Integer> answer = new ArrayList<>();
+        for (int i = 0; i < s.length(); i++) {
+            int c = s.charAt(i);
+            answer.add(c);
+        }
+        for (int i = 97; i <= 122; i++) {
+            if (answer.contains(i)) {
+                System.out.print(answer.indexOf(i) + " ");
+            } else {
+                System.out.print(-1 + " ");
+            }
+        }
+    }
+}
