53008b44aa8833c36cbf437078b65f4a8e830c71
HYUNJUN\sajun <sajun28@naver.com>
2024-01-08 18:49:18 +0900

    Feat: "Array_Partition"

---
+++ src/main/java/week1/day1/Array_Partition.java
@@ -0,0 +1,15 @@
+package week1.day1;
+
+import java.util.Arrays;
+
+public class Array_Partition {
+    public static void main(String[] args) {
+        int[] nums = {1,4,3,2};
+        Arrays.sort(nums);
+        int sum = 0;
+        for (int i = 0; i < nums.length; i += 2) {
+            sum += nums[i];
+        }
+        System.out.println(sum);
+    }
+}
