ed5053d20a52b7091a85cc887a83d33406b91fb2
HYUNJUN\sajun <sajun28@naver.com>
2024-01-08 16:08:41 +0900

    Feat: "Baek_5397"

---
+++ src/main/java/week1/day5/Baek_5397.java
@@ -0,0 +1,41 @@
+package week1.day5;
+
+import java.io.*;
+import java.util.*;
+
+public class Baek_5397 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+        for (int i = 0; i < num; i++) {
+            char[] input = br.readLine().toCharArray();
+            LinkedList<Character> answer = new LinkedList<>();
+            LinkedList<Character> cursor = new LinkedList<>();
+            for (char c : input) {
+                if (c == '<') {
+                    if (!answer.isEmpty()) {
+                        cursor.offerFirst(answer.removeLast());
+                    }
+                } else if (c == '>') {
+                    if (!cursor.isEmpty()) {
+                        answer.offerLast(cursor.pollFirst());
+                    }
+                } else if (c == '-') {
+                    if (!answer.isEmpty()) {
+                        answer.removeLast();
+                    }
+                } else {
+                    answer.offerLast(c);
+                }
+            }
+            StringBuilder result = new StringBuilder();
+            while (!answer.isEmpty()) {
+                result.append(answer.pollFirst());
+            }
+            while (!cursor.isEmpty()) {
+                result.append(cursor.pollFirst());
+            }
+            System.out.println(result);
+        }
+    }
+}
