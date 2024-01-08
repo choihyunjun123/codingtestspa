a71f19fd9478f2905d48ae014f5c52f8d8975217
HYUNJUN\sajun <sajun28@naver.com>
2024-01-08 14:55:21 +0900

    Feat: "oddEvenList"

---
+++ src/main/java/week1/day5/oddEvenList.java
@@ -0,0 +1,19 @@
+package week1.day5;
+
+public class oddEvenList {
+    public static void main(String[] args) {
+//        public ListNode oddEvenList(ListNode head) {
+//            ListNode a = head;
+//            ListNode b = head.next;
+//            ListNode c = b;
+//            while (b != null && b.next != null) {
+//                a.next = b.next;
+//                a = a.next;
+//                b.next = a.next;
+//                b = b.next;
+//            }
+//            // a.next = c;
+//            return a;
+//        }
+    }
+}
