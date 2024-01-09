145663bccd8ae284788973ff1af1d42eedbe30fc
HYUNJUN\sajun <sajun28@naver.com>
2024-01-09 10:34:08 +0900

    Feat: "Baek_2002"

---
+++ src/main/java/week1/day6/Baek_2002.java
@@ -0,0 +1,33 @@
+package week1.day6;
+
+import java.io.*;
+import java.util.*;
+
+public class Baek_2002 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        Deque<String> start = new LinkedList<>();
+        Deque<String> end = new LinkedList<>();
+        for (int i = 0; i < N; i++) {
+            start.push(br.readLine());
+        }
+        for (int i = 0; i < N; i++) {
+            end.push(br.readLine());
+        }
+        int answer = 0;
+        while (!end.isEmpty()) {
+            String expectedCar = start.peekLast();
+            String actualCar = end.peekLast();
+            if (expectedCar.equals(actualCar)) {
+                start.removeLast();
+                end.removeLast();
+            } else {
+                answer++;
+                end.removeLast();
+                start.remove(actualCar);
+            }
+        }
+        System.out.println(answer);
+    }
+}
