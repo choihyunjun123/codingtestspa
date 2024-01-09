d5ecec01868745f15d565af5cad87265bf082adf
HYUNJUN\sajun <sajun28@naver.com>
2024-01-09 11:41:40 +0900

    Feat: "Baek_4949"

---
+++ src/main/java/week1/day6/Baek_4949.java
@@ -0,0 +1,43 @@
+package week1.day6;
+
+import java.io.*;
+import java.util.*;
+
+public class Baek_4949 {
+    public static void main(String[] args) throws IOException {
+        Deque<String> sentences = new LinkedList<>();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String word = br.readLine();
+        while (!word.equals(".")) {
+            sentences.push(word);
+            word = br.readLine();
+        }
+        while (!sentences.isEmpty()) {
+            String sentence = sentences.pollLast();
+            if (isBalanced(sentence)) {
+                System.out.println("yes");
+            } else {
+                System.out.println("no");
+            }
+        }
+    }
+
+    private static boolean isBalanced(String sentence) {
+        Stack<Character> check = new Stack<>();
+        for (int i = 0; i < sentence.length(); i++) {
+            char c = sentence.charAt(i);
+            if (c == '(' || c == '[') {
+                check.push(c);
+            } else if (c == ')' || c == ']') {
+                if (check.isEmpty() ||
+                        (check.peek() == '(' && c == ']') ||
+                        (check.peek() == '[' && c == ')')) {
+                    return false;
+                }
+                check.pop();
+            }
+        }
+        return check.isEmpty();
+    }
+}
