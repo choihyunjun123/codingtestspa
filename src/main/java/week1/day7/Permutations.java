bbbf2839ccebbc5d4961b0c3a0dc0e8b3493552b
HYUNJUN\sajun <sajun28@naver.com>
2024-01-10 12:55:51 +0900

    Feat: "Permutation"

---
+++ src/main/java/week1/day7/Permutations.java
@@ -0,0 +1,30 @@
+package week1.day7;
+
+import java.util.*;
+
+public class Permutations {
+    public static void main(String[] args) {
+        int[] num = {1, 2, 3};
+        List<List<Integer>> answer = new ArrayList<>();
+        List<Integer> subAnswer = new ArrayList<>();
+        boolean[] visited = new boolean[num.length];
+        DFS(num, answer, subAnswer, visited);
+        System.out.println(answer);
+    }
+
+    private static void DFS(int[] num, List<List<Integer>> answer, List<Integer> subAnswer, boolean[] visited) {
+        if (subAnswer.size() == num.length) {
+            answer.add(new ArrayList<>(subAnswer));
+            return;
+        }
+        for (int i = 0; i < num.length; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                subAnswer.add(num[i]);
+                DFS(num, answer, subAnswer, visited);
+                visited[i] = false;
+                subAnswer.remove(subAnswer.size() - 1);
+            }
+        }
+    }
+}
