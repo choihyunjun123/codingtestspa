89ed08e530e1fe486ad77c18102e0edc96423246
HYUNJUN\sajun <sajun28@naver.com>
2024-01-10 14:19:17 +0900

    Feat: "Combination"

---
+++ src/main/java/week1/day7/Combination.java
@@ -0,0 +1,25 @@
+package week1.day7;
+
+import java.util.*;
+
+public class Combination {
+    public static void main(String[] args) {
+        int n = 4;
+        int k = 2;
+
+        List<List<Integer>> answer = new ArrayList<>();
+        DFS(n, k, answer, new ArrayList<>(),0);
+        System.out.println(answer);
+    }
+    private static void DFS(int n, int k, List<List<Integer>> answer, List<Integer> subAnswer, int index) {
+        if (subAnswer.size() == k) {
+            answer.add(new ArrayList<>(subAnswer));
+            return;
+        }
+        for (int i = index; i < n; i++) {
+            subAnswer.add(i + 1);
+            DFS(n, k, answer, subAnswer, ++index);
+            subAnswer.remove(subAnswer.size() - 1);
+        }
+    }
+}
