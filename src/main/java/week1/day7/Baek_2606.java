dccf75e5ccb04080d6e2519796a7187d5f6e1540
HYUNJUN\sajun <sajun28@naver.com>
2024-01-14 17:26:43 +0900

    Feat: "Baek_2606"

---
+++ src/main/java/week1/day7/Baek_2606.java
@@ -0,0 +1,40 @@
+package week1.day7;
+
+import java.io.*;
+import java.util.*;
+
+public class Baek_2606 {
+    static ArrayList<Integer>[] A;
+    static boolean[] visited;
+    static int count = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int comNum = Integer.parseInt(br.readLine());
+        int connectNum = Integer.parseInt(br.readLine());
+        A = new ArrayList[comNum + 1];
+        for (int i = 1; i <= comNum; i++) {
+            A[i] = new ArrayList<>();
+        }
+        for (int i = 1; i <= connectNum; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            A[a].add(b);
+            A[b].add(a);
+        }
+        visited = new boolean[comNum + 1];
+        DFS(1);
+        System.out.println(count);
+    }
+
+    private static void DFS(int i) {
+        visited[i] = true;
+        for (int k : A[i]) {
+            if (!visited[k]) {
+                count++;
+                DFS(k);
+            }
+        }
+    }
+}
