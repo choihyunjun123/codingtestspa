5989604483400f9dd23e982dd6883a1fa763fe07
HYUNJUN\sajun <sajun28@naver.com>
2024-01-15 11:11:12 +0900

    Feat: "Subsets"

---
+++ src/main/java/week1/day8/SubsetsDFS.java
@@ -0,0 +1,24 @@
+package week1.day8;
+
+import java.util.*;
+
+public class SubsetsDFS {
+    static int[] nums = {1, 2, 3};
+    static List<List<Integer>> answer;
+
+    public static void main(String[] args) {
+        answer = new ArrayList<>();
+        List<Integer> subAnswer = new ArrayList<>();
+        DFS(0, subAnswer);
+        System.out.println(answer);
+    }
+
+    private static void DFS(int index, List<Integer> integers) {
+        answer.add(new ArrayList<>(integers));
+        for (int i = index; i < nums.length; i++) {
+            integers.add(nums[i]);
+            DFS(i + 1, integers);
+            integers.remove(integers.size() - 1);
+        }
+    }
+}
