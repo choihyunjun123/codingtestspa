31af719bcb4ff3eeb8fdad5d2a483ca89e8797d5
HYUNJUN\sajun <sajun28@naver.com>
2024-01-15 10:29:57 +0900

    Feat: "Baek_1759"

---
+++ src/main/java/week1/day8/Baek_1759.java
@@ -0,0 +1,44 @@
+package week1.day8;
+
+import java.util.*;
+
+public class Baek_1759 {
+    static char[] vowels = {'a', 'e', 'i', 'o', 'u'};
+    static int L, C;
+    static char[] characters;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        L = scanner.nextInt();
+        C = scanner.nextInt();
+        characters = new char[C];
+        for (int i = 0; i < C; i++) {
+            characters[i] = scanner.next().charAt(0);
+        }
+        Arrays.sort(characters);
+        generatePassword(0, 0, "");
+        scanner.close();
+    }
+
+    private static void generatePassword(int index, int vowelCount, String currentPassword) {
+        if (currentPassword.length() == L) {
+            if (vowelCount >= 1 && (L - vowelCount) >= 2) {
+                System.out.println(currentPassword);
+            }
+            return;
+        }
+        for (int i = index; i < C; i++) {
+            int newVowelCount = isVowel(characters[i]) ? vowelCount + 1 : vowelCount;
+            generatePassword(i + 1, newVowelCount, currentPassword + characters[i]);
+        }
+    }
+
+    private static boolean isVowel(char c) {
+        for (char vowel : vowels) {
+            if (c == vowel) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
